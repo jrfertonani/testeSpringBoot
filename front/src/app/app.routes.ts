5b9ae5aba265a7e629d92ef59423692af6c57bb3
D M I <ademirjuniorfertonani@gmail.com>
2025-02-25 14:42:08 -0300

    rotas

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ProdutosViewComponent } from './components/produtos/produtos-view/produtos-view.component';
+import { ProdutosCreateComponent } from './components/produtos/produtos-create/produtos-create.component';
+import { ProdutosUpdateComponent } from './components/produtos/produtos-update/produtos-update.component';
 
 export const routes: Routes = [
 
   {path:'', component: HomeComponent},
+  {path:'produtos', component: ProdutosViewComponent},
+  {path: 'produtosCreate', component: ProdutosCreateComponent},
+  {path: 'produtosUpdate/:id', component: ProdutosUpdateComponent},
 
 ];
