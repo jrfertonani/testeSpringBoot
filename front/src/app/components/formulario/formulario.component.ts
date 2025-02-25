7f472ebc2a9cfa58df70bb90491e578e165d8a51
D M I <ademirjuniorfertonani@gmail.com>
2025-02-25 16:06:19 -0300

    Formulario

--- front/src/app/components/formulario/formulario.component.ts
+++ front/src/app/components/formulario/formulario.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Produtos } from '../../models/Produtos';
 
 @Component({
   selector: 'app-formulario',
-  imports: [],
+  imports: [RouterModule, FormsModule, ReactiveFormsModule],
   templateUrl: './formulario.component.html',
   styleUrl: './formulario.component.css'
 })
-export class FormularioComponent {
+export class FormularioComponent implements OnInit {
+  @Input() btnAcao!: string;
+
+  @Input() descTitulo!: string;
+
+  @Input() dadosProduto: Produtos | null = null;
+
+  @Output() onSubmit = new EventEmitter<Produtos>();
+
+  produtoForm!: FormGroup;
+
+  ngOnInit(): void {
+    console.log(3);
+
+    this.produtoForm = new FormGroup({
+      id: new FormControl(this.dadosProduto ? this.dadosProduto.id : 0),
+      nome: new FormControl(this.dadosProduto ? this.dadosProduto.descricao : ''),
+      email: new FormControl(this.dadosProduto ? this.dadosProduto.validade : ''),
+      salario: new FormControl(this.dadosProduto ? this.dadosProduto.cadastro : ''),
+      cpf: new FormControl(this.dadosProduto ? this.dadosProduto.preco : ''),
+      situacao: new FormControl(this.dadosProduto ? this.dadosProduto.ativo : true),
+    });
+  }
+
+  submit() {
+    this.onSubmit.emit(this.produtoForm.value);
+  }
 
 }
